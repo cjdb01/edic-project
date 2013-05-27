5bc654cffa3d77a5425c62e877b90caf821edb6f
Christopher Di Bella <cjdibella@bigpond.com>
2013-05-27 23:13:41 +1000

    Save

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -1,18 +1,17 @@
 public class Interpreter
 {
-    public static boolean saveGame(final Sudoku game, final double time)
+    public static boolean saveGame(final Sudoku game, final double time, final String filename)
     {
         StringBuffer buffer = new StringBuffer();
         
-        buffer.append("time = " + Double.toString(time));
-        buffer.appendln();
-        buffer.append("assist = " + Integer.toString(game.getAssist());
-        buffer.appendln();
-        buffer.append("board = { " + game.getBoard(Sudoku.Board.Problem) + " }");
-        buffer.appendln();
+        buffer.append("time = " + Double.toString(time) + "\n");
+        buffer.append("assist = " + Integer.toString(game.getAssist()) + "\n");
+        buffer.append("board = { " + game.getBoard(Sudoku.Board.Problem) + " }\n");
         buffer.append("solution = { " + game.getBoard(true) + " }");
         
-        
+        PrintWriter writer = new PrintWriter(filename, "UTF-8");
+        writer.print(buffer.toString());
+        writer.close();
     }
     
     public static Sudoku loadGame(String filename)
